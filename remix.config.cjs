40e4a61a83cd61c8d54f1a7ce615796c3d5f3fde
BluOwn <teba.741@gmail.com>
2025-05-06 18:23:06 +0400

    up

---
+++ remix.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "cjs",
+  serverPlatform: "node",
+  tailwind: true,
+  postcss: true,
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  serverDependenciesToBundle: [/^marked.*/],
+  publicPath: "/build/",
+  serverBuildPath: "api/index.js",
+  assetsBuildDirectory: "public/build",
+  devServerPort: 3000,
+}; 
